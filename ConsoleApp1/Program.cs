5361b0e76ba1ad9f6cad5f38c3020d873677e224
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2023-07-30 08:38:23 +0500

    Jumping on the Clouds: Revisited
    https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem?h_r=next-challenge&h_v=zen

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,42 +12,32 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
 
-           
-            List<int> a = new List<int> { 5,2,1,3,4};
-            List<int> query = new List<int> { 1,2};
 
-            permutationEquation(a);
-            Console.WriteLine(string.Join(", ", permutationEquation(a)));
+            //List<int> a = new List<int> { 5,2,1,3,4};
+            //List<int> query = new List<int> { 1,2};
+            int[] c = new int[] { 0, 0, 1, 0 };
+
+            var x = jumpingOnClouds(c , 2);
+            //Console.WriteLine(string.Join(", ", permutationEquation(a)));
 
             Console.ReadKey();
         }
-        public static void rotateRight(List<int> a)
+        static int jumpingOnClouds(int[] c, int k)
         {
-            int lastElement = a[a.Count() - 1];
-            for (int i = a.Count() - 1; i > 0; i--)
+            int e = 100;
+            int i = 0;
+            do
             {
-                a[i] = a[i - 1];
-            }
-            a[0] = lastElement;
+                i = (i+k)%c.Length;
+                e--;
+                if (c[i] == 1)
+                {
+                    e -= 2;
+                }
+            } while (i != 0);
 
-
+            return e;
         }
-        public static List<int> permutationEquation(List<int> p)
-        {
-            List<int> res = new List<int>();
-
-            for (int i = 0; i < p.Count(); i++)
-            {
-                int index1 = p.IndexOf(i + 1) + 1;//POS 1 
-                int index2 = p.IndexOf(index1) + 1;//POS 2
-                
-
-                res.Add(index2);
-            }
-            return res;
-
-        }
-
 
     }
 }
