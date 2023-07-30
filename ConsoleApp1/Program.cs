d10ef2c3c1628e8350d384aa0d49f11379b581b1
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2023-07-30 08:46:29 +0500

    Extra Long Factorial

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LanguageExt;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace ConsoleApp1
 {
@@ -16,28 +18,23 @@ namespace ConsoleApp1
             //List<int> a = new List<int> { 5,2,1,3,4};
             //List<int> query = new List<int> { 1,2};
             int[] c = new int[] { 0, 0, 1, 0 };
+                extraLongFactorials(30);
 
-            var x = jumpingOnClouds(c , 2);
+            //var x = jumpingOnClouds(c , 2);
             //Console.WriteLine(string.Join(", ", permutationEquation(a)));
 
             Console.ReadKey();
         }
-        static int jumpingOnClouds(int[] c, int k)
+        public static void extraLongFactorials(int n)
         {
-            int e = 100;
-            int i = 0;
-            do
+            var f = BigInteger.One;
+            for (int i = n; i > 1;i--)
             {
-                i = (i+k)%c.Length;
-                e--;
-                if (c[i] == 1)
-                {
-                    e -= 2;
-                }
-            } while (i != 0);
-
-            return e;
+                f *= i;
+            }
+            Console.WriteLine(f);
         }
+
 
     }
 }
