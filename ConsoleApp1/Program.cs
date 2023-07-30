95a5921c39c19406804aba77910bdac84e6570b6
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2023-07-30 12:52:56 +0500

    Append and Delete https://www.hackerrank.com/challenges/append-and-delete/problem?h_r=next-challenge&h_v=zen

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,21 +18,61 @@ namespace ConsoleApp1
             //List<int> a = new List<int> { 5,2,1,3,4};
             //List<int> query = new List<int> { 1,2};
             int[] c = new int[] { 0, 0, 1, 0 };
-                extraLongFactorials(30);
 
             //var x = jumpingOnClouds(c , 2);
             //Console.WriteLine(string.Join(", ", permutationEquation(a)));
+            Console.WriteLine(appendAndDelete("y", "yu", 2));
+
 
             Console.ReadKey();
         }
-        public static void extraLongFactorials(int n)
+        public static string appendAndDelete(string s, string t, int k)
         {
-            var f = BigInteger.One;
-            for (int i = n; i > 1;i--)
+            int c = 0;
+            while (s[c] == t[c])
             {
-                f *= i;
+                c++;
+                if (c >= s.Length || c >= t.Length)
+                {
+                    break;
+                }
             }
-            Console.WriteLine(f);
+            int lenToRm = s.Length - c;
+            int lenToAdd = t.Length - c;
+            int cost = lenToRm + lenToAdd;
+            int sum = cost - k;
+
+            if (sum == 0)
+            {
+                return "Yes";
+            }
+            else if (sum > 0)
+            {
+                //cost higher than k
+                return "No";
+            }
+            else
+            {
+                int opr = Math.Abs(sum);
+                int totalStringLen = s.Length + t.Length;
+                if (k >= totalStringLen)
+                {
+                    return "Yes";
+                }
+                else if (opr %2 == 0)
+                {
+                    return "Yes";
+                }
+                else
+                {
+                    return "No";
+                }
+            }
+
+
+
+
+
         }
 
 
