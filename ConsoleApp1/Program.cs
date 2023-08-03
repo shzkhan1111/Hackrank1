dd8b0a6d22e1738e00d8260fc851381a91e406aa
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2023-08-03 22:21:10 +0500

    Jumping on the Clouds https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=false&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,43 +22,49 @@ namespace ConsoleApp1
             //Console.WriteLine(string.Join(", ", permutationEquation(a)));
             //Console.WriteLine(appendAndDelete("y", "yu", 2));
 
-            List<int> query = new List<int> { 19 , 10 , 12 ,10 ,24 , 25, 22};
-            Console.WriteLine(repeatedString("a" , 1000000000000).ToString());
+            List<int> query = new List<int> { 0, 1   , 0, 0, 0, 1, 0 };
+            Console.WriteLine(jumpingOnClouds(query).ToString());
             Console.ReadKey();
         }
 
-        public static int findA(string s, long n)
-        {
-            int c = 0;
-            for (int i = 0; i < (int) n; i++)
-            {
-                if (s[i] == 'a')
-                {
-                    c++;
-                }
-            }
-            return c;
-        }
-        public static long repeatedString(string s, long n)
-        {
-            long res = 0;
-            string subs = "";
-            if (s.Length >= n)
-            {
-                subs = s.Substring(0, (int)n);
-                int x = findA(s, n);
-                return x;
-            }
-            else
-            {
-                int a = findA(s, s.Length);
-                long rem = n %  s.Length;
-                int b = findA(s, rem);
 
-                double times = Math.Floor((double)n / (long)s.Length);
-                return a * (long)times + b;
-                //get string count org
-            }
+        public static int jumpingOnClouds(List<int> c)
+        {
+            int res = 0;
+            int indexer = 0;
+            do
+            {
+                if (indexer == c.Count() - 2)
+                {
+                    res++;
+                    indexer++;
+                    break;
+                }
+                if (indexer == c.Count() - 3)
+                {
+                    res++;
+                    indexer += 2;
+                    break;
+                }
+                else if (c[indexer + 2] == 0)
+                {
+                    res++;
+                    indexer += 2;
+                }
+                else if (c[indexer + 1] == 0)
+                {
+                    res++;
+                    indexer++;
+                }
+                else
+                {
+                    res = 0;
+                    break;
+                }
+
+            } while (indexer < c.Count());
+
+            return res;
         }
 
     }
