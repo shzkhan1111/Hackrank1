1618d1ce6a3c0adc23576726099cb17e80737c49
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2023-08-05 21:08:02 +0500

    ACM TEam

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,250 +22,61 @@ namespace ConsoleApp1
             //Console.WriteLine(string.Join(", ", permutationEquation(a)));
             //Console.WriteLine(appendAndDelete("y", "yu", 2));
 
-            List<int> query = new List<int> { 1,2,2,3};
-            List<List<int>> obs = new List<List<int>>();
+            //List<int> query = new List<int> { 1,2,2,3};
+            //List<List<int>> obs = new List<List<int>>();
 
-            obs.Add(new List<int> { 5, 5 });
-            obs.Add(new List<int> { 4, 2 });
-            obs.Add(new List<int> { 2, 3 });
+            //obs.Add(new List<int> { 5, 5 });
+            //obs.Add(new List<int> { 4, 2 });
+            //obs.Add(new List<int> { 2, 3 });
+            List<string> query = new List<string> { "10101"  , "11110"  , "00010"};
+            var dd = acmTeam(query);
+            Console.WriteLine(dd[0].ToString());
+            Console.WriteLine(dd[1].ToString());
 
-            Console.WriteLine(queensAttack(100 , 0 , 1 , 1 , obs).ToString());
             Console.ReadKey();
         }
 
-
-        //public static int queensAttack(int n, int k, int r_q,
-        //    int c_q, List<List<int>> obstacles)
-        //{
-        //    int res = 0;
-
-        //    int setMaxCount = (n+1) * 2;
-
-
-        //    int right_count = setMaxCount;
-        //    int left_count = setMaxCount;
-
-        //    int up_count = setMaxCount;
-        //    int down_count = setMaxCount;
-
-        //    int right_up_count = setMaxCount;
-        //    int left_up_count = setMaxCount;
-
-        //    int right_down_count = setMaxCount;
-        //    int left_down_count = setMaxCount;
-
-        //    int ru = (n - r_q) < (n - c_q) ? (n - r_q) : (n - c_q);
-        //    int lu = (n - r_q) < c_q - 1 ? (n - r_q) : c_q - 1;
-        //    int rb = r_q - 1 < (n - c_q) ? r_q - 1 : (n - c_q);
-        //    int lb = r_q < c_q ? r_q - 1 : c_q - 1;
-
-        //    //settting up counts 
-        //    foreach (var o in obstacles)
-        //    {
-
-        //        if (o[0] == r_q)
-        //        {
-        //            //in the same row up or down 
-        //            if (o[1] < c_q)
-        //            {
-        //                //left
-        //                if (left_count > (c_q - o[1]))
-        //                    left_count = c_q - o[1];
-
-        //            }
-        //            else if (o[1] > c_q)
-        //            {
-        //                //right
-        //                if (right_count > (o[1] - c_q))
-        //                    right_count = o[1] - c_q;
-        //            }
-
-        //        }
-        //        if (o[1] == c_q)
-        //        {
-        //            //up and down
-        //            if (o[0] < r_q)
-        //            {
-        //                //down
-        //                if (down_count > (r_q - o[0]))
-        //                    down_count = r_q - o[0];
-        //            }
-        //            else if (o[0] > r_q)
-        //            {
-        //                //up
-        //                if (up_count > (o[0] - r_q))
-        //                    up_count = o[0] - r_q;
-        //            }
-
-        //        }
-
-
-
-
-
-        //        //diagnols
-        //        if (Math.Abs(o[0] - r_q) == Math.Abs(o[1] - c_q))
-        //        {
-        //            int diff = Math.Abs(o[0] - r_q);
-        //            int rowdif = r_q - o[0];
-        //            int coldif = c_q - o[1];
-        //            if (rowdif < 0 && coldif < 0)
-        //            {
-        //                //left botton
-        //                if (left_down_count > diff)
-        //                    left_down_count = diff;
-        //            }
-        //            else if (rowdif > 0 && coldif < 0)
-        //            {
-        //                //left up
-        //                if (left_up_count > diff)
-        //                    left_up_count = diff;
-        //            }
-        //            else if (rowdif < 0 && coldif > 0)
-        //            {
-        //                //down , right 
-        //                if (right_down_count > diff)
-        //                    right_down_count = diff;
-        //            }
-        //            else if (rowdif > 0 && coldif > 0)
-        //            {
-        //                //up , right 
-        //                if (right_up_count > diff)
-        //                    right_up_count = diff;
-        //            }
-
-
-
-
-
-
-
-        //        }
-
-        //    }
-
-        //    if (right_count == setMaxCount)
-        //    {
-        //        res += (n - c_q);
-        //    }
-        //    else
-        //    {
-        //        res += right_count - 1;
-        //    }
-
-        //    if (left_count == setMaxCount)
-        //    {
-        //        res += (c_q) - 1;
-        //    }
-        //    else
-        //    {
-        //        res += left_count -1 ;
-        //    }
-
-        //    if (up_count == setMaxCount)
-        //    {
-        //        res += (n -r_q);
-        //    }
-        //    else
-        //    {
-        //        res += up_count - 1;
-        //    }
-        //    if (down_count== setMaxCount)
-        //    {
-        //        res += r_q - 1;
-        //    }
-        //    else
-        //    {
-        //        res += down_count  - 1 ;
-        //    }
-        //    //up down 
-
-        //    //find max if no obs
-
-
-        //    if (right_up_count == setMaxCount)
-        //    {
-        //        res += ru;
-        //    }
-        //    else
-        //    {
-        //        res += right_up_count;
-        //    }
-
-        //    if (right_down_count == setMaxCount)
-        //    {
-        //        res += rb;
-        //    }
-        //    else
-        //    {
-        //        res += right_down_count;
-        //    }
-
-        //    if (left_up_count == setMaxCount)
-        //    {
-        //        res += lu;
-        //    }
-        //    else
-        //    {
-        //        res += left_up_count;
-        //    }
-        //    if (left_down_count == setMaxCount)
-        //    {
-        //        res += lb;
-        //    }
-        //    else
-        //    {
-        //        res += left_down_count;
-        //    }
-
-        //    return res;
-        //}
-        public static int queensAttack(int n, int k, int r_q, int c_q, List<List<int>> obstacles)
+        public static List<int> acmTeam(List<string> topic)
         {
-            int res = 0;
-            int setMaxCount = (n + 1) * 2;
-
-            int right_count = n - c_q;
-            int left_count = c_q - 1;
-            int up_count = n - r_q;
-            int down_count = r_q - 1;
-
-            int right_up_count = Math.Min(n - r_q, n - c_q);
-            int left_up_count = Math.Min(n - r_q, c_q - 1);
-            int right_down_count = Math.Min(r_q - 1, n - c_q);
-            int left_down_count = Math.Min(r_q - 1, c_q - 1);
-
-            foreach (var o in obstacles)
+            List<int> longestPair = new List<int>();
+            int pairCount = 0;
+            List<int> res = new List<int>();
+            int count = 0;
+            int max = -1;
+            for(int i = 0; i < topic.Count(); i++)
             {
-                int row = o[0];
-                int col = o[1];
+                for (int j = i+1; j < topic.Count(); j++)
+                {
+                    for (int k = 0; k < topic[j].Length; k++)
+                    {
+                        if (topic[i][k] == '1' || topic[j][k] == '1')
+                        {
+                            count++;
+                        }
+                        
+                    }
+                    //can two form a pair
+                   
+                    
+                    if (count == max)
+                    {
+                        pairCount++;
+                    }
+                    if (count > max)
+                    {
+                        max = count;
+                        pairCount = 1;
+                    }
+                    count = 0;
 
-                // Check if the obstacle is in the same row or column as the queen
-                if (row == r_q)
-                {
-                    if (col < c_q) left_count = Math.Min(left_count, c_q - col - 1);
-                    else if (col > c_q) right_count = Math.Min(right_count, col - c_q - 1);
-                }
-                else if (col == c_q)
-                {
-                    if (row < r_q) down_count = Math.Min(down_count, r_q - row - 1);
-                    else if (row > r_q) up_count = Math.Min(up_count, row - r_q - 1);
-                }
-                else if (Math.Abs(row - r_q) == Math.Abs(col - c_q))
-                {
-                    int diff = Math.Abs(row - r_q);
-                    if (row > r_q && col > c_q) right_up_count = Math.Min(right_up_count, diff - 1);
-                    else if (row > r_q && col < c_q) left_up_count = Math.Min(left_up_count, diff - 1);
-                    else if (row < r_q && col > c_q) right_down_count = Math.Min(right_down_count, diff - 1);
-                    else if (row < r_q && col < c_q) left_down_count = Math.Min(left_down_count, diff - 1);
                 }
             }
 
-            res = right_count + left_count + up_count + down_count +
-                  right_up_count + left_up_count + right_down_count + left_down_count;
-
+            res.Add(max);
+            res.Add(pairCount);
             return res;
         }
+
 
 
     }
