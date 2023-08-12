f3223bca715048baca7a420f32a3fab898ac0f75
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2023-08-12 17:48:07 +0500

    Black white surprise cost

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -28,53 +28,20 @@ namespace ConsoleApp1
             //obs.Add(new List<int> { 5, 5 });
             //obs.Add(new List<int> { 4, 2 });
             //obs.Add(new List<int> { 2, 3 });
-            List<string> query = new List<string> { "10101"  , "11110"  , "00010"};
-            var dd = acmTeam(query);
-            Console.WriteLine(dd[0].ToString());
-            Console.WriteLine(dd[1].ToString());
+            List<string> query = new List<string> { "10101", "11110", "00010" };
+            var dd = taumBday(95677, 39394, 86983, 311224, 311224);
+            Console.WriteLine(dd.ToString());
+            //Console.WriteLine(dd[1].ToString());
 
             Console.ReadKey();
         }
 
-        public static List<int> acmTeam(List<string> topic)
+        public static long taumBday(int b, int w, int bc, int wc, int z)
         {
-            List<int> longestPair = new List<int>();
-            int pairCount = 0;
-            List<int> res = new List<int>();
-            int count = 0;
-            int max = -1;
-            for(int i = 0; i < topic.Count(); i++)
-            {
-                for (int j = i+1; j < topic.Count(); j++)
-                {
-                    for (int k = 0; k < topic[j].Length; k++)
-                    {
-                        if (topic[i][k] == '1' || topic[j][k] == '1')
-                        {
-                            count++;
-                        }
-                        
-                    }
-                    //can two form a pair
-                   
-                    
-                    if (count == max)
-                    {
-                        pairCount++;
-                    }
-                    if (count > max)
-                    {
-                        max = count;
-                        pairCount = 1;
-                    }
-                    count = 0;
+            long blackCost = Math.Min(bc, wc + z);
+            long whiteCost = Math.Min(wc, bc + z);
 
-                }
-            }
-
-            res.Add(max);
-            res.Add(pairCount);
-            return res;
+            return (long)b * blackCost + (long)w * whiteCost;
         }
 
 
