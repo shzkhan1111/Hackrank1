c27edf0cc29b19c4eca2498fa0592c4c91f46b6b
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2023-10-22 19:50:26 +0500

    Leet Code Array Contains Duplicate

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,63 +15,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //var grid = new List<List<char>> {
-            //      new List<char> { '1', '1', '1', '1', '0' },
-            //      new List<char> { '1', '1', '0', '1', '0' },
-            //      new List<char> { '1', '1', '0', '0', '0' },
-            //      new List<char> { '0', '0', '0', '0', '0' }
-            //    };
-            char[][] grid = new char[][] {
-              new char[] { '1', '1', '1', '1', '0' },
-              new char[] { '1', '1', '0', '1', '0' },
-              new char[] { '1', '1', '0', '0', '0' },
-              new char[] { '0', '0', '0', '0', '1' }
-              };
-            NumIslands(grid);
+            int[] num = new int[] { 1, 2, 3, 1 };
+            ContainsDuplicate(num);
             Console.ReadKey();
         }
-
-        public static int NumIslands(char[][] grid)
+        public static bool ContainsDuplicate(int[] nums)
         {
-            int c = 0;
-            for (int i = 0; i < grid.Length; i++)
+            System.Collections.Generic.HashSet<int> set = new System.Collections.Generic.HashSet<int>();
+            for (int i = 0;i < nums.Length;i++)
             {
-                for (int j = 0; j < grid[i].Length; j++)
+                if (set.Contains(nums[i]))
                 {
-                    if (grid[i][j] == '1')
-                    {
-                        //turn it into 0 and see if there  is 1 near by and turn those into 0
-                        c++;
-                        convertTo0(grid, i, j);
-                    }
+                    return true;
                 }
+                set.Add(nums[i]);
             }
 
-            return c;
-
+            return false;
         }
 
-        public static void convertTo0(char[][] grid, int i, int j)
-        {
-            if (i >= grid.Length || i < 0)
-            {
-                return;
-            }
-            if (grid[i].Length <= j || j < 0)
-            {
-                return;
-            }
-            if (grid[i][j] == '0')
-            {
-                return;
-            }
 
-            grid[i][j] = '0';
-            convertTo0(grid, i + 1, j);
-            convertTo0(grid, i - 1, j);
-            convertTo0(grid, i, j + 1);
-            convertTo0(grid, i, j - 1);
-        }
+
 
 
 
