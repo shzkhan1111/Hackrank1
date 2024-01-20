b506c3377c386cc7560e9960e53e30ded494171f
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2024-01-20 22:31:00 +0500

    Composite Join

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,80 +11,68 @@ using System.Xml.Linq;
 
 namespace ConsoleApp1
 {
-    
+
 
     class Program
     {
-        record Person(string FirstName, string LastName);
-        record Pet(string Name, Person Owner);
         record Employee(string FirstName, string LastName, int EmployeeID);
-        record Cat(string Name, Person Owner) : Pet(Name, Owner);
-        record Dog(string Name, Person Owner) : Pet(Name, Owner);
+        record Student(string FirstName, string LastName, int StudentID);
+
+
         static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>()
+{
+    new("Terry", "Adams", 522459),
+    new("Charlotte", "Weiss", 204467),
+    new("Magnus", "Hedland", 866200),
+    new("Vernette", "Price", 437139)
+};
 
-            Person magnus = new(FirstName: "Magnus", LastName: "Hedlund");
-            Person terry = new("Terry", "Adams");
-            Person charlotte = new("Charlotte", "Weiss");
-            Person arlene = new("Arlene", "Huff");
-            Person rui = new("Rui", "Raposo");
+            List<Student> students = new List<Student>()
+{
+    new("Vernette", "Price", 9562),
+    new("Terry", "Earls", 9870),
+    new("Terry", "Adams", 9913)
+};
 
-            List<Person> people = new() { magnus, terry, charlotte, arlene, rui };
 
-            List<Pet> pets = new()
+            var compositeKeyJoinLinq = from e in employees
+                                       join s in students
+                                       on new
+                                       {
+                                           e.FirstName,
+                                           e.LastName
+                                       }
+                                       equals
+                                       new
+                                       {
+                                           s.FirstName,
+                                           s.LastName
+                                       }
+
+                                       select $"{e.FirstName} {e.LastName}";
+
+            foreach (var x in compositeKeyJoinLinq)
             {
-                new(Name: "Barley", Owner: terry),
-                new("Boots", terry),
-                new("Whiskers", charlotte),
-                new("Blue Moon", rui),
-                new("Daisy", magnus),
-            };
-
-            // Create a collection of person-pet pairs. Each element in the collection
-            //// is an anonymous type containing both the person's name and their pet's name.
-            //var query =
-            //    from person in people
-            //    join pet in pets on person equals pet.Owner
-            //    select new
-            //    {
-            //        OwnerName = person.FirstName,
-            //        PetName = pet.Name
-            //    };
-
-            //var result = "";
-            //foreach (var ownerAndPet in query)
-            //{
-            //    result += $"\"{ownerAndPet.PetName}\" is owned by {ownerAndPet.OwnerName}\r\n";
-            //}
-            //Console.Write(result);
-
-
-
-            //var petIsOwnedBy = from p in pets
-            //                   join
-            //                    person in people
-            //                    on p.Owner equals person
-            //                   select new { pet = p, person = person };
-            var petIsOwnedBy =
-                people.Join(
-                    pets,
-                    person => person,
-                    pet => pet.Owner,
-                    (person, pet) => new { pet = pet, person = person});
-
-            foreach (var pet in petIsOwnedBy)
-            {
-                Console.WriteLine($"pet {pet.pet.Name} is owned by {pet.person.FirstName} , {pet.person.LastName}");
+                Console.WriteLine(x);
             }
-            
+            //lambda 
 
-            /* Output:
-                 "Daisy" is owned by Magnus
-                 "Barley" is owned by Terry
-                 "Boots" is owned by Terry
-                 "Whiskers" is owned by Charlotte
-                 "Blue Moon" is owned by Rui
-            */
+            var query = employees.Join(
+                         students,//ordder same 
+                         employee => new { FirstName = employee.FirstName, LastName = employee.LastName },
+                         student => new { FirstName = student.FirstName, student.LastName },
+                         (employee, student) => $"{employee.FirstName} {employee.LastName}"
+                     );
+                foreach (var x in query)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+
 
 
         }
